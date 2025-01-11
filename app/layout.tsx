928ad3ea15b02699803a4b7edb40107acab24c44
Nha Huynh <nha.huynh@zien.vn>
2025-01-11 21:35:13 +0700

    hehe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/app/ui/global.css";
 import { Inter } from "next/font/google";
 
-export const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
